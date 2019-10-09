236009e606084933247c3a9aa8dc41d466e87c55
Tasu Muzinda <muzindatasu@gmail.com>
2019-10-09 12:22:33 +0200

    Added mortality resource

---
+++ zvandiri-buciness/src/main/java/zw/org/zvandiri/business/repo/MortalityRepo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zw.org.zvandiri.business.repo;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import zw.org.zvandiri.business.domain.Mortality;
+import zw.org.zvandiri.business.domain.Patient;
+
+/**
+ *
+ * @author tasu
+ */
+public interface MortalityRepo extends AbstractRepo<Mortality, String>{
+    
+    @Query("from Mortality s left join fetch s.patient left join fetch s.modifiedBy left join fetch s.createdBy where s.patient=:patient")
+    public Mortality findByPatient(@Param("patient") Patient patient);
+}
